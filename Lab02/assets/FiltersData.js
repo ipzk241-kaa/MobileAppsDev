02cb02fe0b082c7d5c01a1ebf1e0dcd875816e9f
Артем Кухар <ipzk241_kaa@student.ztu.edu.ua>
2025-05-29 22:45:22 +0300

    feat: complite Store screen, gamelist, filter

---
+++ Lab02/assets/FiltersData.js
@@ -0,0 +1,20 @@
+const gamesFilters = [
+  "Top Sellers",
+  "Free to play",
+  "Early Access",
+  "Indie",
+  "Massively Multiplayer",
+  "Sports",
+  "Strategy",
+];
+
+const newsFilters = [
+  "All",
+  "Screenshots",
+  "Artwork",
+  "Videos",
+  "Workshop",
+  "News",
+];
+
+export { gamesFilters, newsFilters };
